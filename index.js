2158b7c423e620145cadd3e54a23f4aeb5a10882
+ <+>
2023-03-21 23:37:29 +0300

    Complete assignment

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
-// Code your solution here
+var totalBatteries = 0
+
+function total(){
+    for (let i = 0; i < batteryBatches.length; i++) {
+        sum += batteryBatches[i];
+        totalBatteries = sum;
+        return totalBatteries;
+    }
+    
+}
+batteryBatches.reduce(total);
+
+
+
+
